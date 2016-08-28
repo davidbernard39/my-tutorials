ce54e13cfc65232d8bd526c9be505f165279516e
dav <dav@dav-zenbook.home>
2016-08-28 09:21:20 +0200

    end of step 7

--- angular-phonecat/app/phone-list/phone-list.component.js
+++ angular-phonecat/app/phone-list/phone-list.component.js
@@ -5,22 +5,12 @@ angular.
 module('phoneList').
 component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-        this.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Super rapide',
-                age: 1
-                }, {
-                name: 'Motorolla XOOM avec Wi-Fi',
-                snippet: 'La prochaine génération de tablette',
-                age: 1
-                }, {
-                name: 'Motorolla XOOM',
-                snippet: 'La prochaine génération de tablette',
-                age: 1
-                }
-            ];
-        this.orderProp = 'age';
-    }
+    controller: ['$http', function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data;
+        });
+    }]
 });
--- angular-phonecat/app/phone-list/phone-list.component.spec.js
+++ angular-phonecat/app/phone-list/phone-list.component.spec.js
@@ -1,17 +1,29 @@
 'use strict';
 
 describe('phoneList', function () {
-   
+
     // Load the module that contains the phone list before each test
     beforeEach(module('phoneList'));
-    
+
     describe('PhoneListController', function () {
-         
-        it('should create a `phones` model with 3 phones', inject(function ($componentController) {
-            var ctrl = $componentController('phoneList');
-        
-            expect(ctrl.phones.length).toBe(3);
+        var $httpBackend, ctrl;
+
+        beforeEach(inject(function($componentController, _$httpBackend_) {
+          $httpBackend = _$httpBackend_;
+          $httpBackend.expectGET('phones/phones.json')
+                      .respond([{name: 'Nexus S'}, {name: 'Motorola Droid'}]);
+          ctrl = $componentController('phoneList');
         }));
+
+        it('should create a `phones` model with 2 phones fetched with $http', function () {
+            expect(ctrl.phones).toBeUndefined();
+            $httpBackend.flush();
+            expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola Droid'}]);
+        });
+
+        it('should set a default value for the `orderProp` property', function() {
+          expect(ctrl.orderProp).toBe('age');
+        });
 
     });
 });
