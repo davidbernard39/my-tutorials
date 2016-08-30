4b6039d2f8c60b1de1b740f1053963da1c3b7f1f
Dav <david.bernard39@gmail.com>
2016-08-30 09:08:27 +0200

    end of step 13

--- angular-phonecat/app/phone-detail/phone-detail.component.js
+++ angular-phonecat/app/phone-detail/phone-detail.component.js
@@ -3,17 +3,16 @@
 angular.
   module('phoneDetail').
   component('phoneDetail', {
-      templateUrl: 'phone-detail/phone-detail.template.html',
-      controller: ['$http', '$routeParams', function PhoneDetailController($http, $routeParams) {
-        var self = this;
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$routeParams', 'Phone', function PhoneDetailController($routeParams, Phone) {
+      var self = this;
 
-        self.setImage = function setImage(imageUrl) {
-            self.mainImageUrl = imageUrl;
-        };
+      self.setImage = function setImage(imageUrl) {
+        self.mainImageUrl = imageUrl;
+      };
 
-        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
-          self.phone = response.data;
-          self.setImage(self.phone.images[0]);
-        });
-      }]
+      self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        self.setImage(phone.images[0]);
+      });
+    }]
   });
